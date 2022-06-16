23bb3a31a5ec5dbd703c0ea19c16f460aacfedc4
patel-dhwani <77050044+patel-dhwani@users.noreply.github.com>
2022-06-16 19:16:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCTXVLDiJaw7O-lCaM6OexaDZKuRAYVQRw",
+  authDomain: "story-telling-app-8945e.firebaseapp.com",
+  projectId: "story-telling-app-8945e",
+  storageBucket: "story-telling-app-8945e.appspot.com",
+  messagingSenderId: "600282076720",
+  appId: "1:600282076720:web:0df9755f2e44b6e38f7ff5"
+};
